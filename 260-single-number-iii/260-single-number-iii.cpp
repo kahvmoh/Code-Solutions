c3471b44d91d772575a71e938de2d59e4ed0a2ac
kahvmoh <104728081+kahvmoh@users.noreply.github.com>
2022-05-01 14:38:08 -0600

    Time: 8 ms (84.42%), Space: 10 MB (79.11%) - LeetHub

---
+++ 260-single-number-iii/260-single-number-iii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        unsigned int xorAll = 0;
+        unsigned int mask = 0;
+        vector<int> result(2, -1);
+        
+        if (nums.size() < 2)
+            return result;
+        
+        for (int i = 0; i < nums.size(); i++)
+        {
+            xorAll ^= nums[i];
+        }
+        
+        //get the right most bit that is set
+        mask = xorAll & ~(xorAll - 1);
+        
+        int num1 = 0, num2 = 0;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if(mask & nums[i])
+            {
+                num1 ^= nums[i];
+            }  
+        }
+        
+        num2 = num1 ^ xorAll;
+        
+        result[0] = num1;
+        result[1] = num2;
+        
+        return result;
+    }
+};
