47422fd86cca0698d4947f9a2ed8147897726a34
kahvmoh <104728081+kahvmoh@users.noreply.github.com>
2022-05-04 04:11:58 -0600

    Time: 3 ms (58.97%), Space: 6 MB (34.19%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.cpp
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
     int reverse(int x) {
-        int forward = abs(x);
-        int reversed = 0;
+        int result = 0;
         
-        if( x == (1u << 31))
-            return 0;
-        
-        while ( forward != 0 )
+        while ( x != 0 )
         {
-            int rem = forward % 10;
-            forward /= 10;
+            int rem = x % 10;
+            x /= 10;
            
-            if( ( reversed > numeric_limits<int>::max() / 10 ) || ( reversed == numeric_limits<int>::max() / 10 && rem > 7 ) )
+            if( ( result > INT_MAX / 10 ) || ( result == INT_MAX / 10 && rem > 7 ) )
                  return 0;
-
+            else if( ( result < INT_MIN / 10) || ( result == INT_MIN / 10 && rem < -8 ) )
+                 return 0;
             else
-                reversed = 10 * reversed + rem;
+                result = 10 * result + rem;
         }   
         
-        if ( x < 0)
-            reversed *= -1;
-        
-        return reversed;
+        return result;
     }
 };
