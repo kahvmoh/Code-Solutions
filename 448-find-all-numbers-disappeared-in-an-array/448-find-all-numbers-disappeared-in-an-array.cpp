afa24e2873291950635ae2103a353d675e2f0f13
kahvmoh <104728081+kahvmoh@users.noreply.github.com>
2022-05-06 14:12:14 -0600

    Time: 66 ms (73.71%), Space: 33.5 MB (94.67%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.cpp
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.cpp
@@ -2,21 +2,25 @@ class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
         vector<int> result;
-        int size = nums.size();
-        unordered_set<int> mySet(nums.begin(), nums.end());
+        int n = nums.size();
         
-        //unordered_set<int>::iterator it = mySet.begin();
-        //while ( it != mySet.end() ) 
-        //    cout << *it++ << ", ";
-        
-        for(int i = 1; i <= size; i++) {
-            if(mySet.find(i) == mySet.end()) {
-                cout << "Not found: " << i << ", "; 
-                result.push_back( i );
-            }
-            else
-                cout << "found: " << i << ", "; 
+        // sort the array
+        for( int i = 0; i < n; i++) {
+           while (nums[i] != i+1) {
+               int d = nums[i] - 1;
+               if( nums[i] != nums[d])
+                   swap(nums[i], nums[d]);
+               else
+                   break;
+           } 
         }
+        
+        //look for missing nums
+        for( int i = 0; i < n; i++) {
+            if ( nums[i] != i+1 )
+                result.push_back(i+1);
+        }    
+        
         return result;
     }
 };
