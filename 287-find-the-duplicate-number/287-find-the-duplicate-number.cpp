3e9dc922dfed4cb7a70e064e66c0ed40050f87ab
kahvmoh <104728081+kahvmoh@users.noreply.github.com>
2022-05-06 14:59:58 -0600

    Time: 135 ms (69.27%), Space: 61.2 MB (61.98%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -4,8 +4,8 @@ public:
         int n = nums.size();
         
         for( int i = 0; i < n; i++ ) {
-            while (nums[i] != i + 1) {
-                int d = nums[i] - 1;
+            while (nums[i] != i) {
+                int d = nums[i];
                 if( nums[i] != nums[d] )
                     swap(nums[i], nums[d]);
                 else
@@ -14,7 +14,7 @@ public:
         }
     
         for( int i = 0; i < n; i++)  {
-            if( nums[i] != i + 1)
+            if (nums[i] != i)
                 return nums[i];
         }
         
