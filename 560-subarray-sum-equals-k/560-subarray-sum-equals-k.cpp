27a5fe1325b52fd4831bfe364800d1c558f1595a
kahvmoh <104728081+kahvmoh@users.noreply.github.com>
2022-05-09 15:31:37 -0600

    Time: 97 ms (61.13%), Space: 35.9 MB (97.66%) - LeetHub

---
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int tCount = 0;
+        int pSum = 0;
+        unordered_map<int, int> hmap;
+        hmap[0] = 1;
+        
+        for( int i = 0; i < nums.size(); i++) {
+            pSum += nums[i];
+            
+            // how many prefixes sum to prefixsum - k
+            if( hmap.find(pSum - k) != hmap.end() ) 
+                tCount += hmap[pSum - k];
+            // Update hmap
+            if ( hmap.find( pSum ) != hmap.end() )
+                hmap[pSum]++;
+            else
+                hmap[pSum] = 1;
+        }
+        
+        return tCount;
+    }
+};
