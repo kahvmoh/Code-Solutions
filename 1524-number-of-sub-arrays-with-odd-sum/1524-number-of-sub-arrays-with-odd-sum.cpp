456f7f590a4949cf00df194fb1004e6511aebbf0
kahvmoh <104728081+kahvmoh@users.noreply.github.com>
2022-05-10 10:06:10 -0600

    Time: 179 ms (69.15%), Space: 108 MB (95.08%) - LeetHub

---
+++ 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int numOfSubarrays(vector<int>& arr) {
+        long total = 0;
+        long hmap[2];
+        // keep track of even prefixSum
+        hmap[0] = 1;
+        // keep track of odd prefixSum
+        hmap[1] = 0;
+        long pSum = 0;
+        for( int i = 0; i < arr.size(); i++ ) {
+            pSum += arr[i];
+            
+            if( pSum % 2 == 0 ) 
+                total += hmap[1];
+            else
+                total += hmap[0];
+            
+            // update the counts
+            if( pSum % 2 == 0 ) 
+                hmap[0] += 1;
+            else
+                hmap[1] += 1;
+            
+            total = total % 1000000007;
+        }
+        
+        return total;    
+    }
+};
