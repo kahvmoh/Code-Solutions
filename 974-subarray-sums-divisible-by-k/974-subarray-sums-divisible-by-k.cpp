3992210369097913c6e5aab5911070f56ad67793
kahvmoh <104728081+kahvmoh@users.noreply.github.com>
2022-05-11 08:48:33 -0600

    Time: 76 ms (53.86%), Space: 31.7 MB (54.73%) - LeetHub

--- 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.cpp
+++ 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.cpp
@@ -7,19 +7,20 @@ public:
         hmap[0] = 1;
         
         for(int i = 0; i < nums.size(); i++) {
-            pSum = pSum + nums[i];
-            int rem = pSum % k;
-            if( rem < 0 )
-                rem += k;
-            // Is there any pSum with the same reminder?   
-            if( hmap.find(rem) != hmap.end() )
-                total += hmap[rem];
+            pSum = (pSum + nums[i]) % k;
+
+            if( pSum < 0 )
+                pSum += k;
+            
+            if( hmap.find(pSum) != hmap.end() )
+                total += hmap[pSum];
             
             // Update the hash maap table
-            if( hmap.find(rem) != hmap.end() )
-                hmap[rem] += 1;
+            if( hmap.find(pSum) != hmap.end() )
+                hmap[pSum] += 1;
             else
-                hmap[rem] = 1;
+                hmap[pSum] = 1;
+
         }
                      
         return total;
