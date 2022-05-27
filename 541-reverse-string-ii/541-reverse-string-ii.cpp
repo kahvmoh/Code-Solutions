737bebf0acaa2352663aaece859ba0f987d8093c
kahvmoh <104728081+kahvmoh@users.noreply.github.com>
2022-05-27 10:57:15 -0600

    Time: 8 ms (30.62%), Space: 7.4 MB (32.58%) - LeetHub

--- 541-reverse-string-ii/541-reverse-string-ii.cpp
+++ 541-reverse-string-ii/541-reverse-string-ii.cpp
@@ -1,24 +1,12 @@
 class Solution {
-public:
-    void reverse_str( string &s, int start, int end)
-    {
-        while( start < end ) {
-            char tmp = s[start];
-            s[start] = s[end];
-            s[end] = tmp;
-            start++;
-            end--;
-        }
-    }    
+public:    
         
     string reverseStr(string s, int k) {
         for(int i = 0; i < s.size(); i += 2*k)  {
             if(i+k <= s.size() )
-                reverse_str(s, i, i + k - 1);
-                //reverse(s.begin() + i, s.begin() + i + k);
+                reverse(s.begin() + i, s.begin() + i + k);
             else 
-                reverse_str(s, i, s.size() - 1 );
-                //reverse(s.begin() + i, s.end());
+                reverse(s.begin() + i, s.end());
         }
         return s;
     }
