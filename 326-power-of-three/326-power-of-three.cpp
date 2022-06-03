35e2c66758934726ed5634ed2a0775ed75bc6cc7
kahvmoh <104728081+kahvmoh@users.noreply.github.com>
2022-06-03 10:39:56 -0600

    Time: 30 ms (50.13%), Space: 6 MB (29.06%) - LeetHub

---
+++ 326-power-of-three/326-power-of-three.cpp
@@ -0,0 +1,10 @@
+#include <math.h>
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if ( n <= 0 )
+            return false;
+        int x=(log2(n) / log2(3));
+        return ( pow(3, x) == n );
+    }
+};
