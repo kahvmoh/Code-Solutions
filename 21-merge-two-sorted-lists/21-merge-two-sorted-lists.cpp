5c1f927206b630ce2ae9750b21d167c4ae4dc004
kahvmoh <104728081+kahvmoh@users.noreply.github.com>
2022-07-21 12:13:18 -0600

    Time: 14 ms (40.77%), Space: 14.8 MB (81.00%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -18,13 +18,13 @@ public:
         while( list1 && list2 ) {
             if( list1->val <= list2->val ) {
                 tail->next = list1;
-                tail = list1;
+                tail = tail->next;
                 list1 = list1->next;
                 //tail->next = NULL;
             }
             else {
                 tail->next = list2;
-                tail = list2;
+                tail = tail->next;
                 list2 = list2->next;
                 //tail->next = NULL;
             }
